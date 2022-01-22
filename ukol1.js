aea142208b3314e9eff2db08e7154ad72d965c62
LuciePayne <lucie.theinerova@gmail.com>
2022-01-22 12:25:11 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -9,7 +9,7 @@ console.log(a);
 */
 
 
-let jmeno= 'Petra'
+/* let jmeno= 'Petra'
 let vek=34
 let vaha=65
 let vyska=1.67
@@ -31,7 +31,7 @@ console.log(
   ' a muj BMI je ' + 
   bmi +
   '.'
-)
+)*/
 
 
 
@@ -50,6 +50,46 @@ console.log(a)
 let b = 11
 
 console.log (a+b)*/
+
+//Number ('445')//
+
+/* let odpoved=Number (prompt('Ahoj kolik je ti let?'));
+console.log(odpoved);
+*/
+
+
+/* let jeZima=false;
+let svitiSlunce=true;
+let vysledek=jeZima&&svitiSlunce;
+*/
+
+
+// + - * ** / % & ||  //
+
+/* Přijměte jméno jako vstup od uživatele, dále nechte 
+zadat jeho věk a vypište na základě věku jednu ze 
+dvou vět ve tvaru:
+a) Vaše jméno je Marian, je vám 30 let a tak máte 
+právo řídit auto.
+b) Vaše jméno je Marian, je vám 14 let a tak musíte 
+počkat ještě 4 let, než budete moci řídit auto
+*/
+
+let vek = 30
+
+if (vek=30) {
+
+  console.log('Vaše jméno je Marian, je vám 30 let a tak máte právo řídit auto');
+} else {
+
+  console.log('Vaše jméno je Marian, je vám 14 let a tak musíte počkat ještě 4 let, než budete moci řídit auto');
+  }
+}
+
+
+
+
+
 
 
 
