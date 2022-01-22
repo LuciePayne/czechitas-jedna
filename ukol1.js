147561ccb146ea4fe34b1be194a9e57f27c69886
LuciePayne <lucie.theinerova@gmail.com>
2022-01-22 10:49:28 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -3,5 +3,26 @@
 /* vice radkovy 
 komentar
 */
+let a=11/3
+console.log(a);
+
+
+
 
 console.log('aaa');
+
+//vytvoreni proemnne
+let jmeno='marian'
+let vek=31
+let povolani='programator
+
+let je zima=true
+
+let a = 3>2
+console.log(a)
+let b = 11
+
+console.log (a+b)
+
+
+
